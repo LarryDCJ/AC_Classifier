74d64bd3c6bda4ac7edf660621709b1d71d5f1d4
Larry Cawley <LarryDCJ@icloud.com>
2021-03-25 01:35:05 -0700

    Working through building the model

--- ac_vision_model.py
+++ ac_vision_model.py
@@ -1,2 +1,19 @@
+from fastai.data.all import *
 from fastai.vision.all import *
+from fastai.metrics import error_rate
+from pathlib import Path
+from IPython.display import Image
+
+path = Path("images")
+fnames = get_image_files(path)
+
+datablock = DataBlock()
+
+datasets = datablock.datasets(fnames)
+
+datablock = DataBlock(get_items = get_image_files)
+
+
+
+
 
